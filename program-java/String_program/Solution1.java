79df8f41e84a9cd4c5dc07f83fe0d55252412891
vkanshu <vikanshu9899@gmail.com>
2020-05-04 17:00:51 -1200

    print the first- integer_sum,second-double_sum,third-concat_input_string

---
+++ program-java/String_program/Solution1.java
@@ -0,0 +1,23 @@
+package Question;
+import java.util.*;
+public class Solution1 {
+	 
+		
+	 public static void main(String[] args) {
+	        int i = 4;
+	        double d = 4.0;
+	        String s = "HackerRank ";
+			
+	        Scanner scan = new Scanner(System.in);
+	        scan.nextLine();
+	        String name=scan.nextLine();
+	        int fist=   scan.nextInt();
+	       double second=scan.nextDouble();
+	       
+	       System.out.println(i+fist);
+	       System.out.println(d+second);
+	        System.out.println(s+name);
+	                  
+	 
+	}
+}
