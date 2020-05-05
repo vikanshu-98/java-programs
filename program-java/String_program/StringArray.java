15d0345bbb15f777a367159134882863d50cd704
vkanshu <vikanshu9899@gmail.com>
2020-05-04 16:57:46 -1200

    print the input string

---
+++ program-java/String_program/StringArray.java
@@ -0,0 +1,12 @@
+package Question;
+import java.util.*;
+public class StringArray {
+public static void main(String args[])
+
+{
+	 Scanner in=new Scanner(System.in);
+	 System.out.println("enter the string");
+	 String name=in.nextLine();
+System.out.println(name);
+}
+}
